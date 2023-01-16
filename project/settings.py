eb5e9af6050d352bbc65802d2c67c652b28bacf0
Mateusz Urban <58361072+cyrisek@users.noreply.github.com>
2023-01-16 15:44:25 +0000

    staticfiles2

--- project/settings.py
+++ project/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'game/static/'
+STATIC_URL = 'staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
